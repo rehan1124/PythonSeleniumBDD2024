3f587c641493a5e8bdbf1f449f393bea45ea6d19
Syed Rehan <jrehans@jci.com>
2024-01-20 12:50:37 +0530

    Chrome options

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,9 @@ from selenium import webdriver
 
 def before_scenario(context, scenario):
     url = "https://tutorialsninja.com/demo/"
-    context.driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    context.driver = webdriver.Chrome(options=options)
     context.driver.maximize_window()
     context.driver.implicitly_wait(30)
     context.driver.get(url)
