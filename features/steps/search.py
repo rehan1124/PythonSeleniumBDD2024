23ea71f26a4fec9ef0b4d834c799351157f44510
Syed Rehan <jrehans@jci.com>
2024-01-25 13:28:11 +0530

    Optimizing step definitions

--- features/steps/search.py
+++ features/steps/search.py
@@ -6,35 +6,34 @@ from features.pages.searchpage import Searchpage
 
 @given(u'User enters product in search box')
 def step_impl(context):
-    home_page = Homepage(context.driver)
-    home_page.enter_product_name("HP")
+    context.home_page = Homepage(context.driver)
+    context.home_page.enter_product_name("HP")
 
 
 @given(u'User enters invalid product in search box')
 def step_impl(context):
-    home_page = Homepage(context.driver)
-    home_page.enter_product_name("JALALALALA")
+    context.home_page = Homepage(context.driver)
+    context.home_page.enter_product_name("JALALALALA")
 
 
 @when(u'User clicks on search button')
 def step_impl(context):
-    home_page = Homepage(context.driver)
-    home_page.click_search_button()
+    context.home_page.click_search_button()
 
 
 @then(u'Product should be displayed in search results')
 def step_impl(context):
-    search_page = Searchpage(context.driver)
-    search_page.is_product_displayed("HP LP3065")
+    context.search_page = Searchpage(context.driver)
+    context.search_page.is_product_displayed("HP LP3065")
 
 
 @then(u'Proper message for product not found should be displayed')
 def step_impl(context):
-    search_page = Searchpage(context.driver)
-    search_page.is_invalid_product("There is no product that matches the search criteria.")
+    context.search_page = Searchpage(context.driver)
+    context.search_page.is_invalid_product("There is no product that matches the search criteria.")
 
 
 @given(u'User enters nothing in search box')
 def step_impl(context):
-    home_page = Homepage(context.driver)
-    home_page.enter_product_name("")
+    context.home_page = Homepage(context.driver)
+    context.home_page.enter_product_name("")
